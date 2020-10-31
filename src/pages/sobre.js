c7d4eb51e16c8bcce594e77a1bb579f28447e1db
Kauã Bern <58435477+KauaBern@users.noreply.github.com>
2020-10-30 23:04:28 -0300

    :heavy_plus_sign: Rota sobre adicionada

---
+++ src/pages/sobre.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Cursor from '../components/cursor'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+import '../styles/styles.scss'
+
+const sobrePage = () => {
+    return (
+        <div>
+            <Cursor />
+            <Header />
+            <Footer />
+        </div>
+    )
+};
+
+export default sobrePage;
